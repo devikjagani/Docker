0902c903570dcac34baac1524c71eacd630fe11e
Devik Jagani <39765300+devikjagani@users.noreply.github.com>
2025-09-13 17:34:54 +0530

    python flask app files added.

---
+++ Python Flask app/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, This is Devik Jagani. This is Python Flask app on Docker!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
